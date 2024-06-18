8646d831a5a3328f4f605a83487f52e10577ab07
Nikolai Andronov <nickandronick@gmail.com>
2024-06-18 12:22:35 +0300

    added 3 pages via slots

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot:title>
+        Home page
+    </x-slot>   
+    Home
+</x-layout>
+<x-footer>
+    qwe
+</x-footer>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/home', function () {
-    return "<h1>Привет</h1>";
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
