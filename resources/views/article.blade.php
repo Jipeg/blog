7406c285476af257f7a13607f7002f8196f99830
Nikolai Andronov <nickandronick@gmail.com>
2024-06-18 22:10:45 +0300

    little style for article view

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -3,12 +3,14 @@
         {{$article['title']}}
     </x-slot:heading>
         <h1>{{$article['title']}}</h1>
-        <p>Keywords: {{$article['keywords']}}</p>
+        <p class="italic font-bold">Keywords: {{$article['keywords']}}</p>
         <a href="/articles" class="text-blue-500 hover:underline">
             Go back
         </a>
         <article>
             {!! html_entity_decode($article['body']) !!}
         </article>
-        <p>Created at: {{$article['created_at']}}</p>
+        @if ($article['created_at'])
+            <p class="italic mt-2">Created at: {{$article['created_at']}}</p>
+        @endif
 </x-layout>
